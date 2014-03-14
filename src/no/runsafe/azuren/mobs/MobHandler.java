7d4d31205f2c87903ff43d88a24d7582503a40d7
Kruithne <kruithne@gmail.com>
2014-03-14 06:36:17 +0000

    Interact event was more sensible.

--- src/no/runsafe/azuren/mobs/MobHandler.java
+++ src/no/runsafe/azuren/mobs/MobHandler.java
@@ -1,15 +1,15 @@
 package no.runsafe.azuren.mobs;
 
 import no.runsafe.azuren.WorldHandler;
-import no.runsafe.framework.api.ILocation;
-import no.runsafe.framework.api.event.player.IPlayerMove;
+import no.runsafe.framework.api.event.player.IPlayerInteractEvent;
 import no.runsafe.framework.api.event.plugin.IPluginEnabled;
 import no.runsafe.framework.api.log.IConsole;
 import no.runsafe.framework.api.player.IPlayer;
 import no.runsafe.framework.internal.wrapper.ObjectUnwrapper;
+import no.runsafe.framework.minecraft.event.player.RunsafePlayerInteractEvent;
 import no.runsafe.framework.tools.nms.EntityRegister;
 
-public class MobHandler implements IPluginEnabled, IPlayerMove
+public class MobHandler implements IPluginEnabled, IPlayerInteractEvent
 {
 	public MobHandler(IConsole console)
 	{
@@ -23,15 +23,15 @@ public class MobHandler implements IPluginEnabled, IPlayerMove
 	}
 
 	@Override
-	public boolean OnPlayerMove(IPlayer player, ILocation from, ILocation to)
+	public void OnPlayerInteractEvent(RunsafePlayerInteractEvent event)
 	{
 		console.logInformation("Event detected");
+		IPlayer player = event.getPlayer();
 		if (WorldHandler.playerIsInAzurenWorld(player))
 		{
 			console.logInformation("Player is in correct world.");
 			new Nightstalker(ObjectUnwrapper.getMinecraft(player.getWorld())).spawn(player.getLocation());
 		}
-		return true;
 	}
 
 	private final IConsole console;
