eaac4a397be9312a7734ed51e31b09d2a5762fe9
Kruithne <kruithne@gmail.com>
2014-03-14 19:24:47 +0000

    Remove debugging from WorldHandler.

--- src/no/runsafe/azuren/WorldHandler.java
+++ src/no/runsafe/azuren/WorldHandler.java
@@ -3,7 +3,6 @@ package no.runsafe.azuren;
 import no.runsafe.framework.api.IConfiguration;
 import no.runsafe.framework.api.IWorld;
 import no.runsafe.framework.api.event.plugin.IConfigurationChanged;
-import no.runsafe.framework.api.log.IConsole;
 import no.runsafe.framework.api.player.IPlayer;
 
 import java.util.ArrayList;
@@ -11,11 +10,6 @@ import java.util.List;
 
 public class WorldHandler implements IConfigurationChanged
 {
-	public WorldHandler(IConsole console)
-	{
-		this.console = console;
-	}
-
 	@Override
 	public void OnConfigurationChanged(IConfiguration config)
 	{
@@ -30,14 +24,8 @@ public class WorldHandler implements IConfigurationChanged
 
 	public boolean playerIsInAzurenWorld(IPlayer player)
 	{
-		console.logInformation("Checking worlds!");
-		console.logInformation("World amount: " + worlds.size());
-		for (String worldName : worlds)
-			console.logInformation("World: " + worldName);
-
 		return isAzurenWorld(player.getWorld());
 	}
 
 	private final List<String> worlds = new ArrayList<String>(0);
-	private final IConsole console;
 }
