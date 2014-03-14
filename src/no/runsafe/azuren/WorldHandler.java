2ce3af504d23b3f8bba08c4a927110a2fdab3a39
Kruithne <kruithne@gmail.com>
2014-03-14 18:50:05 +0000

    Debugging!

--- src/no/runsafe/azuren/WorldHandler.java
+++ src/no/runsafe/azuren/WorldHandler.java
@@ -3,6 +3,7 @@ package no.runsafe.azuren;
 import no.runsafe.framework.api.IConfiguration;
 import no.runsafe.framework.api.IWorld;
 import no.runsafe.framework.api.event.plugin.IConfigurationChanged;
+import no.runsafe.framework.api.log.IConsole;
 import no.runsafe.framework.api.player.IPlayer;
 
 import java.util.ArrayList;
@@ -10,6 +11,11 @@ import java.util.List;
 
 public class WorldHandler implements IConfigurationChanged
 {
+	public WorldHandler(IConsole console)
+	{
+		this.console = console;
+	}
+
 	@Override
 	public void OnConfigurationChanged(IConfiguration config)
 	{
@@ -24,8 +30,14 @@ public class WorldHandler implements IConfigurationChanged
 
 	public boolean playerIsInAzurenWorld(IPlayer player)
 	{
+		console.logInformation("Checking worlds!");
+		console.logInformation("World amount: " + worlds.size());
+		for (String worldName : worlds)
+			console.logInformation("World: " + worldName);
+
 		return isAzurenWorld(player.getWorld());
 	}
 
 	private final List<String> worlds = new ArrayList<String>(0);
+	private final IConsole console;
 }
