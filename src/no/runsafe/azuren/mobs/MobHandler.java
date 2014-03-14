d1886cb2689d892dd73697b3a0c2572954858b1b
Kruithne <kruithne@gmail.com>
2014-03-14 06:23:15 +0000

    Switch to use an interact event for debugging.

--- src/no/runsafe/azuren/mobs/MobHandler.java
+++ src/no/runsafe/azuren/mobs/MobHandler.java
@@ -1,14 +1,14 @@
 package no.runsafe.azuren.mobs;
 
 import no.runsafe.azuren.WorldHandler;
-import no.runsafe.framework.api.event.player.IPlayerLeftClickAirEvent;
+import no.runsafe.framework.api.event.player.IPlayerInteractEvent;
 import no.runsafe.framework.api.event.plugin.IPluginEnabled;
 import no.runsafe.framework.api.player.IPlayer;
 import no.runsafe.framework.internal.wrapper.ObjectUnwrapper;
-import no.runsafe.framework.minecraft.event.player.RunsafePlayerClickEvent;
+import no.runsafe.framework.minecraft.event.player.RunsafePlayerInteractEvent;
 import no.runsafe.framework.tools.nms.EntityRegister;
 
-public class MobHandler implements IPluginEnabled, IPlayerLeftClickAirEvent
+public class MobHandler implements IPluginEnabled, IPlayerInteractEvent
 {
 	@Override
 	public void OnPluginEnabled()
@@ -17,7 +17,7 @@ public class MobHandler implements IPluginEnabled, IPlayerLeftClickAirEvent
 	}
 
 	@Override
-	public void OnPlayerLeftClick(RunsafePlayerClickEvent event)
+	public void OnPlayerInteractEvent(RunsafePlayerInteractEvent event)
 	{
 		IPlayer player = event.getPlayer();
 		if (WorldHandler.playerIsInAzurenWorld(player))
